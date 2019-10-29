2b4c5df4ca621703c008e0dca05a68f49c6a5a16
Alejandro Hernández <Alejandrehl@icloud.com>
2019-10-29 17:53:38 -0400

    Multiples avances

---
+++ components/ToggeableTimerForm.js
@@ -0,0 +1,16 @@
+import React from "react";
+import {View, StyleSheet, Text} from "react-native";
+
+const ToggeableTimerForm = () => {
+    return (
+        <View>
+            <Text>
+                Toggeable Timer Form
+            </Text>
+        </View>
+    );
+};
+
+const styles = StyleSheet.create({});
+
+export default ToggeableTimerForm;
