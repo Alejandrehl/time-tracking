00b985fcc40817cea342de653b599230fc66d04b
Alejandro Hernández <Alejandrehl@icloud.com>
2019-10-29 22:30:40 -0400

    Iniciando estados hardcoded

--- App.js
+++ App.js
@@ -1,10 +1,30 @@
-import React from 'react';
+import React, {useState} from 'react';
 import {StyleSheet, Text, View, ScrollView} from 'react-native';
+
+import uuidv4 from "uuid/v4";
 
 import EditableTimer from "./components/EditableTimer";
 import ToggleableTimerForm from "./components/ToggeableTimerForm";
 
+
 const App = () => {
+    const [timers, setTimers] = useState([
+        {
+            title: "Mow the lawn",
+            project: "House Chores",
+            id: uuidv4(),
+            elapsed: 5456099,
+            isRunning: true
+        },
+        {
+            title: "Bake Squash",
+            project: "Kitchen Chores",
+            id: uuidv4(),
+            elapsed: 1273998,
+            isRunning: false
+        }
+    ]);
+
     return (
         <View style={styles.appContainer}>
             <View style={styles.titleContainer}>
--- readme.md
+++ readme.md
@@ -1,1 +1,4 @@
 # Time Tracking App
+
+- yarn add uuid: This library generates ids for each of our timers. The library's function uuidv4() will randomly generate a Universally Unique IDentifier for each of our timers.
+    * A UUID is a string that looks like this: 2030efbd-a32-4fcc-8637-7c410896b3e3
